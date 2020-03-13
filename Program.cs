c0075d57d1b871f5d758eb45261a3fc3dd962ead
Bárbara <babyr.oliveira@gmail.com>
2020-03-13 19:57:04 -0300

    Escrevi a letra da música, conforme o exercício proposto.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Música
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("My Chemical Romance - I Don't Love You");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Well, when you go");
+            Console.ReadKey();
+            Console.WriteLine("So never think I'll make you try to stay");
+            Console.ReadKey();
+            Console.WriteLine("And maybe when you get back");
+            Console.ReadKey();
+            Console.WriteLine("I'll be off to find another way");
+            Console.ReadKey();
+            Console.WriteLine("When after all this time that you still owe");
+            Console.ReadKey();
+            Console.WriteLine("You're still a good-for-nothing I don't know");
+            Console.ReadKey();
+            Console.WriteLine("So take your gloves and get out");
+            Console.ReadKey();
+            Console.WriteLine("Baby, get out while you can");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("When you go");
+            Console.ReadKey();
+            Console.WriteLine("And would you even turn to say");
+            Console.ReadKey();
+            Console.WriteLine("I don't love you like I did yesterday");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Sometimes I cry so hard from pleading");
+            Console.ReadKey();
+            Console.WriteLine("So sick and tired of all the needless beating");
+            Console.ReadKey();
+            Console.WriteLine("But baby when they knock you down and out");
+            Console.ReadKey();
+            Console.WriteLine("Is where you oughta stay");
+            Console.ReadKey();
+            Console.WriteLine("And after all the blood that you still owe");
+            Console.ReadKey();
+            Console.WriteLine("Another time was just another blow");
+            Console.ReadKey();
+            Console.WriteLine("So fix your eyes and get up");
+            Console.ReadKey();
+            Console.WriteLine("Baby, get up while you can");
+
+        }
+    }
+}
